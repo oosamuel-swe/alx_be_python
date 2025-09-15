88c27b84ae40452ef6dfba0711ea94cc516fe458
Obasi Onyekachi Samuel <thesamsg@gmail.com>
2025-09-15 20:04:15 +0000

    Age calculator

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,9 @@
+
+# Ask the user for their current age
+current_age = int(input("How old are you? "))
+
+# Calculate age in 2050 (2023 → 2050 is 27 years difference)
+future_age = current_age + 27
+
+# Print the result in the required format
+print("In 2050, you will be", future_age, "years old.")
